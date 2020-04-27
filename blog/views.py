6d9018ef9989f1809d33934b3980486196f0febf
ortegamdx <victor.hdez.ort@gmail.com>
2020-04-26 23:50:17 -0400

    sdfsdfadsf

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
+from blog.models import Post
 from blog.forms import PostForm
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
@@ -8,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def post_list(request):
    
-    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
